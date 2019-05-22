8481868759e1c86c5cf0e866ae2a6e1775dfad86
Ortis <ortis@ortis.io>
2019-05-22 19:01:01 +0900

    new GUI

---
+++ src/main/java/org/ortis/jsafebox/gui/MetadataPanel.java
@@ -0,0 +1,17 @@
+package org.ortis.jsafebox.gui;
+
+import javax.swing.*;
+
+/**
+ * @author Ortis
+ */
+public class MetadataPanel
+{
+	private JPanel panel1;
+	private JTable table1;
+
+	private void createUIComponents()
+	{
+		// TODO: place custom component creation code here
+	}
+}
---
+++ src/main/java/org/ortis/jsafebox/gui/SafeExplorerPanel.java
@@ -0,0 +1,11 @@
+package org.ortis.jsafebox.gui;
+
+import javax.swing.*;
+
+/**
+ * @author Ortis
+ */
+public class SafeExplorerPanel
+{
+	private JPanel panel1;
+}
